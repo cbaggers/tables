8ac1b3133e41c00b3e22ee11c4bcbbd993f6433f
cbaggers <chris.bagley@gmail.com>
2018-09-09 12:40:51 +0200

    basic constraint checking. lets look at parameterized types next

--- types/test-lang.lisp
+++ types/test-lang.lisp
@@ -5,3 +5,10 @@
 (define-ttype (unordered-set type))
 (define-ttype (unordered-foo type size)
   :where ((size integer)))
+
+(define-ttype disposable
+  :purpose :constraint-only
+  :satifies-this-p (lambda (this type-ref)
+                     (format t "ooo! ~a ~a"
+                             this type-ref)
+                     t))
