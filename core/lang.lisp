b98c00c44cde8e5ca45ad6c360085b2fd839a8f6
cbaggers <chris.bagley@gmail.com>
2018-11-10 14:23:38 +0100

    prep for functions

--- core/lang.lisp
+++ core/lang.lisp
@@ -1,6 +1,11 @@
 (in-package :tables-lang)
 
 ;;------------------------------------------------------------
+;; Acronyms
+;;
+;; ssad - single static assingment'ed -> ssa'd -> ssad
+;;
+;;------------------------------------------------------------
 
 (defvar *registered-user-types* (make-hash-table :test #'eq))
 (defvar *registered-parameter-types* (make-hash-table :test #'eq))
@@ -124,29 +129,38 @@
 
 (defclass blockify-context ()
   ((parent :initarg :parent)
-   (bindings :initform nil :initarg :bindings)))
+   (bindings :initform nil :initarg :bindings)
+   (functions :initform nil :initarg :functions)))
 
-(defun make-blockify-context (parent new-bindings)
-  (let ((res (make-instance 'blockify-context
-                            :parent parent
-                            :bindings (when parent
-                                        (slot-value parent 'bindings)))))
+(defun make-blockify-context (parent new-bindings new-functions)
+  (let ((res (make-instance
+              'blockify-context
+              :parent parent
+              :bindings (when parent
+                          (slot-value parent 'bindings))
+              :functions (when parent
+                           (slot-value parent 'functions)))))
     (with-slots (bindings) res
       (loop
          :for binding :in new-bindings
          :do (setf bindings (cons binding bindings))))
+    (with-slots (functions) res
+      (loop
+         :for function :in new-functions
+         :do (setf functions (cons function functions))))
     res))
 
 (defun test ()
   (let ((res (infer (make-check-context 'tables)
-                    `(funcall (lambda ((a ?a))
+                    `(funcall (lambda ((a ?a) (i integer))
                                 (let ((b a))
                                   (if b
-                                      b
-                                      b)))
-                              t))))
+                                      i
+                                      20)))
+                              t
+                              10))))
     (print res)
-    (let* ((context (make-blockify-context nil nil))
+    (let* ((context (make-blockify-context nil nil nil))
            (lets (blockify context res))
            (last (first (last lets))))
       `(let* ,lets
@@ -210,15 +224,18 @@
                    :for (name val) :in renamed-args
 
                    :for blocked := (blockify context val)
-                   :for ssad-name := (first (first (last blocked)))
+                   :for last := (first (last blocked))
+                   :for ssad-name := (first last)
                    :append blocked
-                   :collect (list name ssad-name)))
+                   :collect (list name `(truly-the ,(second (second last))
+                                                   ,ssad-name))))
          (context (make-blockify-context
                    context
                    (loop
                       :for (old-name) :in (second form)
                       :for (new-name) :in renamed-args
-                      :collect (cons old-name new-name))))
+                      :collect (cons old-name new-name))
+                   nil))
          (body (third form))
          (lets (blockify context body))
          (ssad-name (first (first (last lets)))))
@@ -242,7 +259,8 @@
                    (loop
                       :for (old-name) :in (second form)
                       :for (new-name) :in renamed-args
-                      :collect (cons old-name new-name))))
+                      :collect (cons old-name new-name))
+                   nil))
          (body (third form))
          (lets (blockify context body))
          (ssad-name (first (first (last lets)))))
@@ -264,28 +282,108 @@
      (cons (first expr-ast) ssad-names)
      prior-lets)))
 
+;; (let* ((#:g1172
+;;         (truly-the #t(function (boolean) boolean)
+;;                    (lambda ((#:a_1162 #tboolean))
+;;                      (let* ((#:g1164 (truly-the #tboolean #:a_1162))
+;;                             (#:b_1163 #:g1164)
+;;                             (#:g1168
+;;                              (truly-the #tboolean
+;;                                         (if (#:g1165 (truly-the #tboolean #:b_1163))
+;;                                             (let* ((#:g1166 (truly-the #tboolean #:b_1163)))
+;;                                               (truly-the #tboolean #:g1166))
+;;                                             (let* ((#:g1167 (truly-the #tboolean #:b_1163)))
+;;                                               (truly-the #tboolean #:g1167)))))
+;;                             (#:g1169 (truly-the #tboolean #:g1168))
+;;                             (#:g1170 (truly-the #tboolean #:g1169))
+;;                             (#:g1171 (truly-the #tboolean #:g1164)))
+;;                        (truly-the #tboolean #:g1171)))))
+;;        (#:g1173 (truly-the #tboolean t))
+;;        (#:g1174 (truly-the #tboolean (funcall #:g1172 #:g1173))))
+;;   (truly-the #tboolean #:g1174))
 
-;; (labels ((foo ((#:g1076 #tboolean))
-;;            (truly-the #tboolean #:g1076))
-;;          (bar ((#:g1077 #tboolean))
-;;            (truly-the #tboolean #:g1077))
-;;          (lam ((#:a_1072 ?a))
-;;            (let* ((#:g1074 (truly-the #tboolean #:a_1072))
-;;                   (#:b_1073 #:g1074)
-;;                   (#:g1078 (truly-the
-;;                             #tboolean
-;;                             (if (#:g1075 (truly-the #tboolean #:b_1073))
-;;                                 (foo #:b_1073)
-;;                                 (bar #:b_1073))))
-;;                   (#:g1079 (truly-the #tboolean #:g1078))
-;;                   (#:g1080 (truly-the #tboolean #:g1079))
-;;                   (#:g1081 (truly-the #tboolean #:g1074)))
-;;              (truly-the #tboolean #:g1081)))
-;;          (baz ()
-;;            (let* ((#:g1082 (truly-the #t(function (boolean) boolean)
-;;                                       #'lam))
-;;                   (#:g1083 (truly-the #tboolean t))
-;;                   (#:g1084 (truly-the #tboolean
-;;                                       (funcall #:g1082 #:g1083))))
-;;              (truly-the #tboolean #:g1084))))
-;;   (baz))
+;; (let* ((#:g1173 (truly-the #tboolean t))
+;;        (#:a_1162 #:g1173)
+;;        (#:g1164 (truly-the #tboolean #:a_1162))
+;;        (#:b_1163 #:g1164)
+;;        (#:g1168
+;;         (truly-the #tboolean
+;;                    (if (#:g1165 (truly-the #tboolean #:b_1163))
+;;                        (let* ((#:g1166 (truly-the #tboolean #:b_1163)))
+;;                          (truly-the #tboolean #:g1166))
+;;                        (let* ((#:g1167 (truly-the #tboolean #:b_1163)))
+;;                          (truly-the #tboolean #:g1167)))))
+;;        (#:g1169 (truly-the #tboolean #:g1168))
+;;        (#:g1170 (truly-the #tboolean #:g1169))
+;;        (#:g1171 (truly-the #tboolean #:g1164))
+;;        (#:g1174 (truly-the #tboolean #:g1171)))
+;;   (truly-the #tboolean #:g1174))
+
+;; (labels ((a ()
+;;            (let* ((#:g1173 (truly-the #tboolean t))
+;;                   (#:a_1162 #:g1173)
+;;                   (#:g1164 (truly-the #tboolean #:a_1162))
+;;                   (#:b_1163 #:g1164))
+;;              (if (truly-the #tboolean #:b_1163)
+;;                  (b #:b_1163)
+;;                  (c #:b_1163))))
+;;          (b ((#:g1166 #tboolean))
+;;            (d (truly-the #tboolean #:g1166)))
+;;          (c ((#:g1167 #tboolean))
+;;            (d (truly-the #tboolean #:g1167)))
+;;          (d (#:g1168 #tboolean)
+;;            (let* ((#:g1169 (truly-the #tboolean #:g1168))
+;;                   (#:g1170 (truly-the #tboolean #:g1169))
+;;                   (#:g1171 (truly-the #tboolean #:g1164))
+;;                   (#:g1174 (truly-the #tboolean #:g1171)))
+;;              (truly-the #tboolean #:g1174))))
+;;   (a))
+
+;; (labels ((a ()
+;;            (let* ((#:b_1163 (truly-the #tboolean t)))
+;;              (if #:b_1163
+;;                  (b #:b_1163)
+;;                  (c #:b_1163))))
+;;          (b ((#:g1166 #tboolean))
+;;            (d (truly-the #tboolean #:g1166)))
+;;          (c ((#:g1167 #tboolean))
+;;            (d (truly-the #tboolean #:g1167)))
+;;          (d (#:g1168 #tboolean)
+;;            (let* ((#:g1174 :g1168))
+;;              (truly-the #tboolean #:g1174))))
+;;   (a))
+
+;; (labels ((a ()
+;;            (if (truly-the #tboolean t)
+;;                (b (truly-the #tboolean t))
+;;                (c (truly-the #tboolean t))))
+;;          (b ((#:g1166 #tboolean))
+;;            (d (truly-the #tboolean #:g1166)))
+;;          (c ((#:g1167 #tboolean))
+;;            (d (truly-the #tboolean #:g1167)))
+;;          (d (#:g1168 #tboolean)
+;;            (truly-the #tboolean #:g1168)))
+;;   (a))
+
+;; (labels ((a ()
+;;            (b (truly-the #tboolean t)))
+;;          (b ((#:g1166 #tboolean))
+;;            (d (truly-the #tboolean #:g1166)))
+;;          (c ((#:g1167 #tboolean))
+;;            (d (truly-the #tboolean #:g1167)))
+;;          (d (#:g1168 #tboolean)
+;;            (truly-the #tboolean #:g1168)))
+;;   (a))
+
+;; (labels ((b ((#:g1166 #tboolean))
+;;            (d (truly-the #tboolean #:g1166)))
+;;          (d (#:g1168 #tboolean)
+;;            (truly-the #tboolean #:g1168)))
+;;   (b (truly-the #tboolean t)))
+
+;; (labels ((d (#:g1168 #tboolean)
+;;            (truly-the #tboolean #:g1168)))
+;;   (d (truly-the #tboolean t)))
+
+;; (labels ()
+;;   (truly-the #tboolean t))
