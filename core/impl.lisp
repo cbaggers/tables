5e8b4d68d037bf144d21b62e93921e6a5f22a1b3
cbaggers <chris.bagley@gmail.com>
2019-02-10 12:01:22 +0100

    add define macros for records, value-types & layouts

--- core/impl.lisp
+++ core/impl.lisp
@@ -2,10 +2,50 @@
 
 ;;------------------------------------------------------------
 
+(defclass aggregate-spec ()
+  ((name :initarg :name :reader aggregate-name)
+   (slots :initarg :slots :reader aggregate-slots)))
+
+(defclass value-type-spec (aggregate-spec)
+  ((size :initarg :size :reader value-size)))
+
+(defclass aggregate-slot ()
+  ((name :initarg :name :reader slot-name)
+   (type :initarg :type :reader slot-type)))
+
+(defmethod make-load-form ((obj aggregate-spec)
+                           &optional environment)
+  (declare (ignore environment))
+  (with-slots (name slots) obj
+    `(make-instance 'aggregate-spec
+                    :name ',name
+                    :slots ,(cons 'list slots))))
+
+(defmethod make-load-form ((obj value-type-spec)
+                           &optional environment)
+  (declare (ignore environment))
+  (with-slots (name size slots) obj
+    `(make-instance 'value-type-spec
+                    :name ',name
+                    :size size
+                    :slots ,(cons 'list slots))))
+
+(defmethod make-load-form ((obj aggregate-slot)
+                           &optional environment)
+  (declare (ignore environment))
+  (with-slots (name type) obj
+    `(make-instance 'aggregate-slot
+                    :name ',name
+                    :type ,type)))
+
+;;------------------------------------------------------------
+
 (defvar *registered-user-types* (make-hash-table :test #'eq))
 (defvar *registered-parameter-types* (make-hash-table :test #'eq))
 (defvar *registered-constraints* (make-hash-table :test #'eq))
 (defvar *registered-top-level-functions* (make-hash-table :test #'eq))
+(defvar *registered-records* (make-hash-table :test #'eq))
+(defvar *registered-value-types* (make-hash-table :test #'eq))
 
 ;;------------------------------------------------------------
 
@@ -28,6 +68,16 @@
   (format t "~%;; Registered function ~a" func-name)
   (setf (gethash func-name *registered-top-level-functions*)
         (generalize type)))
+
+(defun register-record (spec)
+  (let ((name (aggregate-name spec)))
+    (format t "~%;; Registered layout ~a" name)
+    (setf (gethash name *registered-records*) spec)))
+
+(defun register-value-type (spec)
+  (let ((name (aggregate-name spec)))
+    (format t "~%;; Registered layout ~a" name)
+    (setf (gethash name *registered-value-types*) spec)))
 
 ;;------------------------------------------------------------
 
@@ -59,10 +109,12 @@
 ;;------------------------------------------------------------
 
 (defclass spec-data ()
-  ((traits :initform (make-hash-table) :initarg :traits)))
+  ((traits :initform (make-hash-table) :initarg :traits)
+   (layout :initarg :layout)))
 
-(defun make-spec-data ()
-  (make-instance 'spec-data))
+(defun make-spec-data (layout)
+  (assert (or (null layout) (typep layout 'aggregate-spec)))
+  (make-instance 'spec-data :layout layout))
 
 (defmethod make-load-form ((obj spec-data)
                            &optional environment)
@@ -76,7 +128,7 @@
 
 (defmacro define-ttype (designator
                         &body rest
-                        &key where)
+                        &key where aggregate-info)
   (declare (ignore rest))
   (destructuring-bind (name . rest) (uiop:ensure-list designator)
     (declare (ignore rest))
@@ -84,7 +136,7 @@
                  (make-ttype-spec (find-type-system 'tables)
                                   designator
                                   where
-                                  (make-spec-data)))))
+                                  (make-spec-data aggregate-info)))))
       `(progn
          (register-type ,spec)
          ',name))))
@@ -100,7 +152,7 @@
                                        designator
                                        where
                                        satifies-this-p
-                                       (make-spec-data)))))
+                                       (make-spec-data nil)))))
       `(progn
          (register-constraint ,spec)
          ',name))))
@@ -127,3 +179,60 @@
     (register-top-level-function name spec)
     `(progn
        (register-top-level-function ',name ,spec))))
+
+;;------------------------------------------------------------
+
+(defun parse-slot (definition)
+  (destructuring-bind (name type)
+      (if (numberp definition)
+          (list nil definition)
+          definition)
+    ;; {TODO} proper checks
+    (assert (symbolp name))
+    (assert (symbol-package name))
+    (make-instance 'aggregate-slot
+                   :name name
+                   :type type)))
+
+(defmacro define-record (name &body slots)
+  (let ((spec (make-instance
+               'aggregate-spec
+               :name name
+               :slots (mapcar #'parse-slot slots))))
+    (register-record spec)
+    `(progn
+       (register-record ,spec)
+       (define-ttype ,name :aggregate-info ,spec)
+       ',name)))
+
+#+nil
+(define-record ff32
+  (sign 1)
+  (mantissa 7)
+  (exponent 23))
+
+;;------------------------------------------------------------
+
+(defmacro define-value-type (name (size) &body slots)
+  (let ((spec (make-instance
+               'value-type-spec
+               :name name
+               :size size
+               :slots (mapcar #'parse-slot slots))))
+    (register-value-type spec)
+    `(progn
+       (register-value-type ,spec)
+       (define-ttype ,name :aggregate-info ,spec)
+       ',name)))
+
+;;------------------------------------------------------------
+
+;; Defining a layout is simply defining a function that takes
+;; a struct definition and returns a new one, potentially with
+;; additional padding slots
+
+(defmacro define-layout (name (aggregate-arg) &body body)
+  (declare (ignore aggregate-arg body))
+  ;; (let ((layout-name (intern )))
+  ;;   `(defun ,name () ))
+  `',name)
