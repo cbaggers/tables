04617bb5017ecfca00920f9ad80daeab74a02f26
cbaggers <chris.bagley@gmail.com>
2019-03-10 15:21:37 +0100

    quick tidy up of the compile test code

--- core/compile.lisp
+++ core/compile.lisp
@@ -1,31 +1,19 @@
 (in-package :tables.compile)
 
-(defun test-pass-1 (code)
+(defun first-run (code)
   (let ((ast (infer (make-check-context 'tables) code)))
-    (tables.compile.stage-0.ast-to-ir:run-pass ast)))
-
-(defun test-pass-2 (code)
-  (tables.compile.stage-0.early-constant-folding:run-pass
-   (tables.compile.stage-0.dead-binding-removal:run-pass
-    (test-pass-1 code))))
-
-(defun test-pass-3 (code)
-  (tables.compile.stage-0.inline-direct-calls:run-pass
-   (tables.compile.stage-0.dead-binding-removal:run-pass
-    (test-pass-2 code))))
-
-(defun test-pass-4 (code)
-  (tables.compile.stage-0.dead-binding-removal:run-pass
-   (tables.compile.stage-0.dead-if-branch-removal:run-pass
-    (test-pass-3 code))))
-
-(defun test-pass-5 (code)
-  (tables.compile.stage-0.inline-top-level-functions:run-pass
-   (test-pass-4 code)))
+    (tables.compile.stage-0.inline-top-level-functions:run-pass
+     (tables.compile.stage-0.dead-binding-removal:run-pass
+      (tables.compile.stage-0.dead-if-branch-removal:run-pass
+       (tables.compile.stage-0.early-constant-folding:run-pass
+        (tables.compile.stage-0.inline-direct-calls:run-pass
+         (tables.compile.stage-0.dead-binding-removal:run-pass
+          (tables.compile.stage-0.early-constant-folding:run-pass
+           (tables.compile.stage-0.dead-binding-removal:run-pass
+            (tables.compile.stage-0.ast-to-ir:run-pass ast)))))))))))
 
 (defun test (&optional code)
-  (let ((hi (tables.compile.stage-0.early-constant-folding:run-pass
-             (test-pass-5 code))))
+  (let ((hi (first-run code)))
     (loop
        :for i :below 10
        :do (setf
