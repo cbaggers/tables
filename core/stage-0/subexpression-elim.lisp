23ba0a01ff12fac0a96fb9930755da794e271a9f
cbaggers <chris.bagley@gmail.com>
2019-04-02 14:14:30 +0200

    lift a form out of an 'if' if it exists in both branches
    This then lets this eliminate with forms in the parent block, which is
    nice.

--- core/stage-0/subexpression-elim.lisp
+++ core/stage-0/subexpression-elim.lisp
@@ -7,15 +7,25 @@
   ssad-let)
 
 (defmethod s-elim ((o ssad-let1) env)
-  ;;
+  ;; returns a list of keys
   (with-slots (bindings body-form) o
-    (loop
-       :for b :in bindings
-       :for new-pairs := (s-elim-binding b env)
-       :when new-pairs
-       :do (setf env (append new-pairs env)))
-    (s-elim body-form env)
-    (values)))
+    (let ((keys
+           (loop
+              :for b :in bindings
+              :for key :=
+                (multiple-value-bind (new-pairs key)
+                    (s-elim-binding b env)
+                  (when new-pairs
+                    (setf env (append new-pairs env))
+                    key))
+              :collect key)))
+      (setf bindings
+            (loop :for binding :in bindings
+               :for new-bindings := (s-elim (slot-value binding 'form) env)
+               :append new-bindings
+               :collect binding))
+      (s-elim body-form env)
+      keys)))
 
 (defun form-key (binding-name form)
   (typecase form
@@ -49,33 +59,58 @@
   (with-slots (form name) o
     (multiple-value-bind (key record-accessor-pairs)
         (form-key name form)
-      (when key
-        (let ((existing-form (assocr key env :test #'equal)))
-          (if existing-form
-              (etypecase existing-form
-                (ssad-binding
-                 (setf form (make-instance
-                             'ssad-var :binding existing-form))
-                 nil)
-                (ssad-constant
-                 (setf form existing-form)
-                 nil))
-              (cons (cons key o) record-accessor-pairs)))))))
+      (if key
+          (let ((existing-form (assocr key env :test #'equal)))
+            (if existing-form
+                (etypecase existing-form
+                  (ssad-binding
+                   (setf form (make-instance
+                               'ssad-var :binding existing-form))
+                   (values nil key))
+                  (ssad-constant
+                   (setf form existing-form)
+                   (values nil key)))
+                (values (cons (cons key o) record-accessor-pairs)
+                        key)))
+          (values nil key)))))
 
 (defmethod s-elim ((o ssad-lambda) env)
   (with-slots (body-form) o
     (s-elim o env)
-    (values)))
+    nil))
 
 (defmethod s-elim ((o ssad-if) env)
+  ;; returns bindings to inject into parent
   (with-slots (test then else) o
     (s-elim test env)
-    (s-elim then env)
-    (s-elim else env)
-    (values)))
+    (let* ((then-keys (s-elim then env))
+           (else-keys (s-elim else env)))
+      (loop
+         :for tpos :from 0
+         :for tkey :in then-keys
+         :for tbinding :in (slot-value then 'bindings)
+         :for ebindings := (slot-value else 'bindings)
+         :for epos := (position tkey else-keys :test #'equal)
+         :when epos
+         :collect
+           (let* ((tform (slot-value tbinding 'form))
+                  (ttype (slot-value tbinding 'type))
+                  (tunif (slot-value tbinding 'is-uniform))
+                  (new-binding
+                   (make-instance 'ssad-binding
+                                  :name (gensym)
+                                  :form tform
+                                  :type ttype
+                                  :is-uniform tunif))
+                  (ebinding (nth epos ebindings)))
+             (setf (slot-value tbinding 'form)
+                   (make-instance 'ssad-var :binding new-binding))
+             (setf (slot-value ebinding 'form)
+                   (make-instance 'ssad-var :binding new-binding))
+             new-binding)))))
 
-(defmethod s-elim ((o ssad-funcall) env) (values))
-(defmethod s-elim ((o ssad-var) env) (values))
-(defmethod s-elim ((o symbol) env) (values))
-(defmethod s-elim ((o ssad-constant) env) (values))
-(defmethod s-elim ((o ssad-constructed) env) (values))
+(defmethod s-elim ((o ssad-funcall) env) nil)
+(defmethod s-elim ((o ssad-var) env) nil)
+(defmethod s-elim ((o symbol) env) nil)
+(defmethod s-elim ((o ssad-constant) env) nil)
+(defmethod s-elim ((o ssad-constructed) env) nil)
