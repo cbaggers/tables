32dc2c500a0ee4cb0cc2aaf20d994a22ae74b084
cbaggers <chris.bagley@gmail.com>
2019-04-04 12:08:36 +0200

    cleanup in compile tests

--- core/compile.lisp
+++ core/compile.lisp
@@ -1,20 +1,16 @@
 (in-package :tables.compile)
 
-(defun first-pass (ir)
-  (tables.compile.stage-0.inline-top-level-functions:run-pass
-   (tables.compile.stage-0.dead-binding-removal:run-pass
-    (tables.compile.stage-0.dead-if-branch-removal:run-pass
-     (tables.compile.stage-0.early-constant-folding:run-pass
-      (tables.compile.stage-0.inline-direct-calls:run-pass
-       (tables.compile.stage-0.dead-binding-removal:run-pass
-        (tables.compile.stage-0.early-constant-folding:run-pass
-         (tables.compile.stage-0.dead-binding-removal:run-pass
-          ir)))))))))
-
 (defun first-run (ctx code)
   (let ((ast (infer ctx code)))
-    (first-pass
-     (tables.compile.stage-0.ast-to-ir:run-pass ast))))
+    (tables.compile.stage-0.inline-top-level-functions:run-pass
+     (tables.compile.stage-0.dead-binding-removal:run-pass
+      (tables.compile.stage-0.dead-if-branch-removal:run-pass
+       (tables.compile.stage-0.early-constant-folding:run-pass
+        (tables.compile.stage-0.inline-direct-calls:run-pass
+         (tables.compile.stage-0.dead-binding-removal:run-pass
+          (tables.compile.stage-0.early-constant-folding:run-pass
+           (tables.compile.stage-0.dead-binding-removal:run-pass
+            (tables.compile.stage-0.ast-to-ir:run-pass ast)))))))))))
 
 (defun test (&optional code uniforms)
   (let* ((ctx (make-check-context 'tables))
@@ -39,20 +35,52 @@
                     hi))))))))))
     hi))
 
+#+nil
+;; BUG: infinite loop in print (well designator-from-type really)
+;;      as the type is properly recursive! The are type is the
+;;      function type. I love that the checker was happy to do this
+;;      it's rather mind bending.
+;;      I guess we need a *print-circle* for types XD. or actually..
+;;      we just need to make the designator circular, and punt to
+;;      CL for the printing shiz.
+(infer 'tables
+       '(lambda ((f (function ((function (?a) ?b)) ?b)))
+          (funcall f f)))
+
 ;; {TODO}
 ;;
+;; - code hoisting
 ;; - funcall sinking (push funcall of conditional result to branches)
 ;; - make and & or special forms?
-;; - integer ops
-;; - code hoisting
 ;; - add arg name to arg bindings
+;; - integer ops
+;; - query if var/form depends on var (lets us do limited lamdba fold)
+;; - track number of things depending on binding?
 ;;
 ;; Amongst others..
 
 #+nil
-(defun ftest ()
-  (tables.compile::test
-   '(funcall (if (checkmate.lang:construct boolean ??)
-                 (lambda ((x i8)) (+ x x))
-                 (lambda ((x i8)) (* x x)))
-     1)))
+(test '(let ((f (if a
+                    (lambda ((x i8)) (* x x))
+                    (lambda ((x i8)) (+ x x)))))
+        (+ (funcall f 10)
+         (funcall f 20)))
+      '((a boolean)))
+
+#||
+
+funcalls of ifs returning lambdas is still super interesting. One thing
+I need to work out is, when is there ever other bindings in an 'if' branch
+which also returns a lambda?
+
+Anything that isnt returned, or isnt depended on by the thing returned is
+stripped. That means the other thing needs to be used by the lambda.
+
+This means closures.
+
+Now we have no mutation so once it's captured it wont change. This feels
+like it gives us an opportunity for simplifying the handling here.
+
+
+
+||#
