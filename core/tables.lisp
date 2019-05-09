a6416fa73855f7021657bc0a4022f7ba17e48896
cbaggers <chris.bagley@gmail.com>
2019-05-09 16:18:55 +0200

    parse the table forms

--- core/tables.lisp
+++ core/tables.lisp
@@ -10,33 +10,37 @@
    (qualities :initform nil :initarg :qualities)))
 
 (defclass table-spec ()
-  ((column-specs :initarg :column-specs)))
+  ((column-specs :initarg :column-specs)
+   (validated :initform nil)))
 
-(defclass but-chunk ()
+(defclass chunk ()
   ((columns :initarg columns)))
 
-(defclass but-column ()
+(defclass column ()
   ((pointer :initarg :pointer)
+   (first-element-pointer :initarg :first-element-pointer)
    (count :initarg :count)))
 
-(defclass basic-unsorted-table ()
+(defclass unordered-table ()
   ((chunks :initarg :chunks)))
 
+;;------------------------------------------------------------
+
+(defun make-column-spec (column-form)
+  (destructuring-bind (name type-designator) column-form
+    (make-instance 'column-spec
+                   :name name
+                   :type-designator type-designator)))
+
+(defun make-table-spec (column-forms)
+  (let ((column-specs (mapcar #'make-column-spec column-forms)))
+    (make-instance 'table-spec :column-specs column-specs)))
+
+;;------------------------------------------------------------
+
 ;; First version restrictions are
-;; - not implement filter yet, only map
+;; - implementing map & delete
+;; - only supporting delete within chunk, no cross chunk stuff movement yet
 ;; - no aos columns
 ;; - no struct columns (we need to add output destructuring to optimizer first)
 ;; - no column qualities (e.g. cluster)
-
-
-;; We will come back to these when we understand how to the the above stuff.
-#+nil
-(progn
-  (defclass cluster (column-quality) ())
-  (defclass but-cluster ()
-    ((chunks :initarg :chunks)))
-  (defclass basic-unsorted-clustered-table ()
-    ((clusters :initarg :clusters))))
-
-
-;;------------------------------------------------------------
